5ce63d9d6e8f3a2d667a0625817cf2e812dd0348
rafael-longeville <58341489+rafael-longeville@users.noreply.github.com>
2022-12-08 15:25:55 +0100

    sidebar route api fix

--- nego-front/pages/_app.tsx
+++ nego-front/pages/_app.tsx
@@ -1,11 +1,26 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Work Sans', sans-serif`,
+    body: `'Work Sans', sans-serif`,
+  },
+  colors: {
+    primary: {
+      300: "#7B849A",
+      500: "#333645",
+      700: "#272938"
+    }
+  }
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
     </>
