50a54e111637318b034f6d1f5cfa3eebf00c2370
rafael-longeville <58341489+rafael-longeville@users.noreply.github.com>
2022-12-08 15:26:16 +0100

    push

--- negosud.api/Program.cs
+++ negosud.api/Program.cs
@@ -22,22 +22,29 @@ builder.Services.AddScoped<IArticleService, ArticleService>();
 //builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<NegoSudDbContext>();
+
+
+builder.Services.AddMvc();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "Cors",
+                      policy =>
+                      {
+                          policy.AllowAnyHeader();
+                          policy.AllowAnyOrigin();
+                          policy.WithOrigins("http://localhost:3000");
+                          policy.AllowAnyMethod();
+                      });
+});
+builder.Services.AddAutoMapper(typeof(UserMapping));
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<NegoSudDbContext>(option =>
 {
     option.UseSqlServer(builder.Configuration.GetConnectionString("negoSudDb"));
 });
-builder.Services.AddControllers();
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-
-builder.Services.AddMvc();
-builder.Services.AddCors();
-builder.Services.AddAutoMapper(typeof(UserMapping));
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -46,9 +53,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("Cors");
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
