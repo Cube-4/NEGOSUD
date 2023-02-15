aa0760e4235d71710484b420c43911210e47031f
Rafael Longeville <rafael.longeville.pro@gmail.com>
2023-02-15 11:56:04 +0100

    datagrid fixes

--- nego-frontz/src/pages/stocks/index.tsx
+++ nego-frontz/src/pages/stocks/index.tsx
@@ -1,7 +1,42 @@
-export default function Stocks() {
+import React from "react";
+import dynamic, { type DynamicOptions } from "next/dynamic";
+import "@inovua/reactdatagrid-community/index.css";
+import { columns } from "./columns";
+import type TypeDataGridProps from "@inovua/reactdatagrid-community/types/TypeDataGridProps";
+
+//---- import component as dynamic with un poco de bricolaje --
+const DynamicDataGrid = dynamic(
+  (() => {
+    return import("@inovua/reactdatagrid-community");
+  }) as DynamicOptions<Partial<TypeDataGridProps>>,
+  {
+    ssr: false,
+  }
+);
+
+//---- Create your page using the dynamic component ------------
+export default function Page({ data }: any) {
   return (
     <div>
       <h1>Stocks</h1>
+      <DynamicDataGrid
+        idProperty="id"
+        columns={columns}
+        dataSource={data}
+        defaultLimit={10}
+        style={{ minHeight: 400 }}
+      />
     </div>
   );
+}
+
+//---- Bonus: Make your request serverSide ---------------------
+export async function getServerSideProps() {
+  const products = await fetch("http://localhost:44312/api/article").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: { data: products },
+  };
 }
