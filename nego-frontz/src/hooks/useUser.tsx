5667e3f7e26c82f0cd02d571efb0832270b5634b
Rafael Longeville <rafael.longeville.pro@gmail.com>
2023-04-18 07:08:50 +0200

    ajout user

--- nego-frontz/src/hooks/useUser.tsx
+++ nego-frontz/src/hooks/useUser.tsx
@@ -1,35 +1,28 @@
-import { useMutation } from "react-query";
 import axios from "axios";
 import authHeader from "@/helpers/auth-headers";
 
-export default function useUser() {
-  interface userData {
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    role: string;
-  }
-  async function user({ firstName, lastName, email, password }: userData) {
-    const data = {
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      password: password,
-    };
-    console.log(data);
-    try {
-      const response = await axios.post(
-        "http://localhost:44312/api/user",
-        data,
-        {
-          headers: authHeader(),
-        }
-      );
-      console.log("Hello IS " + response.data);
-    } catch (error: any) {
-      alert(error);
-    }
-  }
-  return useMutation(user);
+interface userData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  role: number;
+}
+
+export default function useUser(userData: userData) {
+  const rolesList = [];
+  rolesList.push(userData.role);
+  console.log(rolesList);
+
+  const data = {
+    firstName: userData.firstName,
+    lastName: userData.lastName,
+    email: userData.email,
+    password: userData.password,
+    role: rolesList,
+  };
+
+  return axios.post("http://localhost:44312/api/user", data, {
+    headers: authHeader(),
+  });
 }
