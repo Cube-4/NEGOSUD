c883564f876d983e37b19dfbdb2cf3bc5e643cae
Rafael Longeville <rafael.longeville.pro@gmail.com>
2023-04-18 07:08:50 +0200

    ajout user

--- nego-frontz/src/components/Clients/ClientsAdd.tsx
+++ nego-frontz/src/components/Clients/ClientsAdd.tsx
@@ -3,109 +3,89 @@ import {
   Text,
   Flex,
   Button,
-  Select,
   TextInput,
-  Box,
+  NumberInput,
+  PasswordInput,
 } from "@mantine/core";
 import { useStyles } from "./styles";
 // Form handling
 import { useForm } from "react-hook-form";
 import { showNotification } from "@mantine/notifications";
 import useUser from "@/hooks/useUser";
+import { useRef, useState } from "react";
 
 export default function () {
   const { classes } = useStyles();
   // Form handling
-  const { mutate: user, isLoading, isError } = useUser();
-  const { register, handleSubmit } = useForm();
-  async function onSubmit(data: any) {
-    user({
-      firstName: data.firstName,
-      lastName: data.lastName,
-      password: data.password,
-      email: data.email,
-      role: data.role,
-    });
+  const [firstName, setFirstName] = useState<string>();
+  const [lastName, setLastName] = useState<string>();
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [role, setRole] = useState<any>();
+
+  async function onSubmit() {
+    if (firstName && lastName && email && password && role) {
+      useUser({
+        firstName,
+        lastName,
+        email,
+        password,
+        role,
+      });
+    }
   }
+
   return (
     <Paper shadow="xl" className={classes.paper}>
       <Text mb={{ base: "2vh" }}>Passer une commande au fournisseur</Text>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Flex gap={{ base: "5vw" }} w="100%">
-          <Flex gap={{ base: "1vh" }} className={classes.inputFlex}>
-            <TextInput
-              label="Prénom"
-              placeholder="Exemple : John"
-              classNames={classes}
-              {...register("firstName")}
-            />
-            <TextInput
-              label="Nom de famille"
-              placeholder="Exemple : Doe"
-              classNames={classes}
-              {...register("lastName")}
-            />
-          </Flex>
-          <Flex gap={{ base: "1vh" }} className={classes.inputFlex}>
-            <TextInput
-              label="E-mail"
-              placeholder="Exemple : johndoe@gmail.com"
-              classNames={classes}
-              {...register("email")}
-            />
-            <TextInput
-              label="Mot de passe"
-              classNames={classes}
-              {...register("password")}
-            />
-          </Flex>
-          <Flex gap={{ base: "1vh" }} className={classes.inputFlex}>
-            <TextInput
-              label="Role de l'utilisateur"
-              placeholder="Exemple : 1"
-              classNames={classes}
-              {...register("role")}
-            />
-            <Button fullWidth mt="xl" disabled={isLoading} type="submit">
-              {isLoading ? "Loading..." : "Ajouter l'utilisateur"}
-            </Button>
-          </Flex>
+      <Flex gap={{ base: "5vw" }} w="100%">
+        <Flex gap={{ base: "1vh" }} className={classes.inputFlex}>
+          <TextInput
+            label="Prénom"
+            placeholder="Exemple : John"
+            classNames={classes}
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <TextInput
+            label="Nom de famille"
+            placeholder="Exemple : Doe"
+            classNames={classes}
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
         </Flex>
-      </form>
-    </Paper>
-  );
-}
+        <Flex gap={{ base: "0.5vh" }} className={classes.inputFlex}>
+          <TextInput
+            label="E-mail"
+            placeholder="Exemple : johndoe@gmail.com"
+            classNames={classes}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-export function TextAndSelect({
-  classes,
-  textLabel,
-  selectLabel,
-  textPlaceholder,
-  selectPlaceholder,
-  selectData,
-  textInputType,
-  register,
-}: any) {
-  return (
-    <>
-      <Box>
-        <Select
-          data={selectData}
-          placeholder={selectPlaceholder}
-          label={selectLabel}
-          classNames={classes}
-          //   {...register("article")}
-        />
-      </Box>
-      <Box w="100%">
-        <TextInput
-          type={textInputType}
-          label={textLabel}
-          placeholder={textPlaceholder}
-          classNames={classes}
-          {...register("quantity")}
-        />
-      </Box>
-    </>
+          <PasswordInput
+            label="Password"
+            value={password}
+            onChange={(event) => setPassword(event.currentTarget.value)}
+          />
+        </Flex>
+        <Flex gap={{ base: "1vh" }} className={classes.inputFlex}>
+          <NumberInput
+            size="md"
+            min={0}
+            max={2}
+            defaultValue={0}
+            placeholder="Your age"
+            withAsterisk
+            label="Role"
+            onChange={setRole}
+          />
+          <Button fullWidth onClick={onSubmit}>
+            Ajouter l'utilisateur
+          </Button>
+        </Flex>
+      </Flex>
+    </Paper>
   );
 }
