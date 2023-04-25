146a25cd2dda9ca60247e9f8d4aa13b1e4a54a20
Rafael Longeville <rafael.longeville.pro@gmail.com>
2023-04-25 08:36:51 +0200

    payment system end to end

--- nego-frontz/src/pages/api/checkout_sessions.js
+++ nego-frontz/src/pages/api/checkout_sessions.js
@@ -1,22 +1,26 @@
+import NextCors from "nextjs-cors";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
+  await NextCors(req, res, {
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+  // Allow CORS
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   if (req.method === "POST") {
     try {
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: "price_1N0Q7PIlm4reS1I61EUcAAzM",
-            quantity: 1,
-          },
-        ],
+        line_items: req.body,
         mode: "payment",
-        success_url: `${req.headers.origin}/?success=true`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
+        success_url: `http://localhost:3000/cart?success=true`,
+        cancel_url: `http://localhost:3000/cart?canceled=true`,
       });
-      res.redirect(303, session.url);
+      res.json({ url: session.url }); // <-- this is the changed line
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
---
+++ nego.DataAccess/Migrations/20230424183332_article-update.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace nego.dataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class articleupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StripePriceId",
+                table: "Articles",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StripeProductId",
+                table: "Articles",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StripePriceId",
+                table: "Articles");
+
+            migrationBuilder.DropColumn(
+                name: "StripeProductId",
+                table: "Articles");
+        }
+    }
+}
