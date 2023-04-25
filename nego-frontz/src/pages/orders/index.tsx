04f284074d2718cda4f1f9cf376a78fbb4d7d490
lNalator <enzomidonetpro@gmail.com>
2023-04-25 12:21:11 +0200

    EMidonet : fixed user getting all orders (now getting his own) added ConfirmOrder for Admin

--- nego-frontz/src/pages/orders/index.tsx
+++ nego-frontz/src/pages/orders/index.tsx
@@ -1,7 +1,7 @@
-import React, { useCallback, useContext, useState } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import dynamic, { type DynamicOptions } from "next/dynamic";
 import "@inovua/reactdatagrid-community/index.css";
-import { columns } from "./columns";
+import { adminColumns, columns } from "./columns";
 import type TypeDataGridProps from "@inovua/reactdatagrid-community/types/TypeDataGridProps";
 import CheckBox from "@inovua/reactdatagrid-community/packages/CheckBox";
 
@@ -10,10 +10,6 @@ import { OrderAdd } from "@/components/Orders";
 import { Button, Group } from "@mantine/core";
 import ReactDataGrid from "@inovua/reactdatagrid-community";
 import authProtected from "@/components/authProtected";
-import {
-  TypeRowSelection,
-  TypeDataSource,
-} from "@inovua/reactdatagrid-community/types";
 import axios from "axios";
 import authHeader from "@/helpers/auth-headers";
 import { useRouter } from "next/router";
@@ -29,16 +25,52 @@ const DynamicDataGrid = dynamic(
 
 function Page({ data }: any) {
   const isAdmin = localStorage.getItem("isAdmin") === "true";
+  const userId = localStorage.getItem("id");
+  const [userOrder, setUserOrder] = useState([]);
   const [selected, setSelected] = useState({});
+  const [isSelectedConfirmed, setIsSelectedConfirmed] = useState(false);
   const router = useRouter();
+
+  useEffect(() => {
+    const fetchData = async (id: string | null) => {
+      const response = await axios.get(
+        `http://localhost:44312/api/user/${id}`,
+        {
+          headers: authHeader(),
+        }
+      );
+      setUserOrder(response.data.orders);
+    };
+    fetchData(userId);
+  }, []);
 
   const onSelectionChange = useCallback(({ selected: selectedMap }: any) => {
     setSelected(selectedMap);
+    const test = (selectedMap: { [x: number]: { orderStatus: string } }) => {
+      let testArray = [];
+      for (let id in selectedMap) {
+        testArray.push(selectedMap[id].orderStatus);
+      }
+      if (testArray.includes("Confirmed")) {
+        return true;
+      }
+      return false;
+    };
+    setIsSelectedConfirmed(test(selectedMap));
   }, []);
 
   const deleteId = async (selected: object) => {
     for (let id in selected) {
       await axios.delete(`http://localhost:44312/api/order/${id}`, {
+        headers: authHeader(),
+      });
+    }
+    router.replace(router.asPath);
+  };
+
+  const confirmOrder = async (selected: object) => {
+    for (let id in selected) {
+      await axios.post(`http://localhost:44312/api/order/confirmOrder/${id}`, {
         headers: authHeader(),
       });
     }
@@ -53,7 +85,7 @@ function Page({ data }: any) {
           <OrderAdd />
           <ReactDataGrid
             idProperty="id"
-            columns={columns}
+            columns={adminColumns}
             checkboxColumn={true}
             dataSource={data}
             sortable={true}
@@ -63,7 +95,13 @@ function Page({ data }: any) {
             defaultLimit={10}
             style={{ minHeight: 400 }}
           />
-          <Buttons selected={selected} deleteId={deleteId} setSelected={setSelected}/>
+          <Buttons
+            selected={selected}
+            deleteId={deleteId}
+            confirmOrder={confirmOrder}
+            test={isSelectedConfirmed}
+            setSelected={setSelected}
+          />
         </>
       )}
       {!isAdmin && (
@@ -71,12 +109,8 @@ function Page({ data }: any) {
           <ReactDataGrid
             idProperty="id"
             columns={columns}
-            checkboxColumn={true}
-            dataSource={data}
+            dataSource={userOrder}
             sortable={true}
-            multiSelect={true}
-            selected={selected}
-            onSelectionChange={(e) => onSelectionChange(e)}
             defaultLimit={10}
             style={{ minHeight: 400 }}
           />{" "}
@@ -94,10 +128,29 @@ const Buttons = (props: any) => {
       <Button
         radius="md"
         style={{ flex: 1 }}
+        color="red"
         disabled={props.selected && Object.keys(props.selected).length === 0}
-        onClick={() => {props.deleteId(props.selected); props.setSelected({})}}
+        onClick={() => {
+          props.deleteId(props.selected);
+          props.setSelected({});
+        }}
       >
         Delete
+      </Button>
+      <Button
+        radius="md"
+        style={{ flex: 1 }}
+        disabled={
+          (props.selected && Object.keys(props.selected).length === 0) ||
+          (props.selected && props.test === true) ||
+          (props.selected && Object.keys(props.selected).length > 1)
+        }
+        onClick={() => {
+          props.confirmOrder(props.selected);
+          props.setSelected({});
+        }}
+      >
+        Confirm order
       </Button>
     </Group>
   );
